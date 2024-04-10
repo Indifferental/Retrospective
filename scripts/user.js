73c583702129c619aa7561202dca1441ea2005bc
Indifferental <70567512+Indifferental@users.noreply.github.com>
2024-04-10 19:43:14 +0700

    Update user.js

--- scripts/user.js
+++ scripts/user.js
@@ -1,16 +1,15 @@
 // ==UserScript==
-// @name         Retrospective
+// @name         Retrospective User
 // @namespace    http://tampermonkey.net/
 // @version      none
 // @description  none
 // @author       https://vk.com/drawingwithblood
 // @match        https://*.tankionline.com/*
-// @icon         https://i.imgur.com/c4YqBSx.png
+// @icon         https://github.com/Indifferental/Retrospective/blob/main/source/logo.png?raw=true
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
-GM_xmlhttpRequest ({
-    url: "https://raw.githubusercontent.com/Indifferental/Retrospective/main/scripts/main.js",
-    method: "GET",
-    onload: (ev) => {eval(ev.responseText);
-    }});
+    GM_xmlhttpRequest ({
+        url: "https://raw.githubusercontent.com/Indifferental/Retrospective/main/scripts/main.js",
+        method: "GET",
+        onload: (ev) => { eval(ev.responseText) }})
