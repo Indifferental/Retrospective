fc57bab2521d12752cd3476ffb2f7de7ef002a77
Indifferental <70567512+Indifferental@users.noreply.github.com>
2024-04-10 16:20:55 +0700

    Update user.js

--- scripts/user.js
+++ scripts/user.js
@@ -5,12 +5,12 @@
 // @description  none
 // @author       https://vk.com/drawingwithblood
 // @match        https://*.tankionline.com/*
-// @icon         https://cdn.discordapp.com/attachments/1139415558505382031/1223231509205352488/retrospectivelogo.png?ex=66191a1e&is=6606a51e&hm=667216536a535451b726b4f732d5bc491328422a672985ab5ccc688f461e803e&
+// @icon         https://i.imgur.com/c4YqBSx.png
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
 GM_xmlhttpRequest ({
-    url: "https://github.com/Indifferental/Retrospective/blob/main/scripts/main.js",
+    url: "https://raw.githubusercontent.com/Indifferental/Retrospective/main/scripts/main.js?token=GHSAT0AAAAAACNOBPXWK44RKPE6BAWNPIW4ZQWLICQ",
     method: "GET",
     onload: (ev) => {eval(ev.responseText);
     }});
