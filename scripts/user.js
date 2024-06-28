9819b7d4d803cd6773250813b1ed1501fb3db423
Indifferental <70567512+Indifferental@users.noreply.github.com>
2024-06-28 12:34:06 +0700

    Update user.js

--- scripts/user.js
+++ scripts/user.js
@@ -5,11 +5,8 @@
 // @description  none
 // @author       https://vk.com/drawingwithblood
 // @match        https://*.tankionline.com/*
-// @icon         https://raw.githubusercontent.com/Indifferental/Retrospective/main/assets/logo.webp
+// @icon         https://raw.githubusercontent.com/Indifferental/Retrospective/main/assets/newlogo.webp
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
-GM_xmlhttpRequest ({
-    url: "https://raw.githubusercontent.com/Indifferental/Retrospective/main/scripts/main.js",
-    method: "GET",
-    onload: (ev) => { eval(ev.responseText) } })
+GM_xmlhttpRequest ({ url: "https://raw.githubusercontent.com/Indifferental/Retrospective/main/scripts/main.js", method: "GET", onload: (ev) => { eval(ev.responseText) } });
