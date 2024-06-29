747f03781bb9bf860dcddbc487e9fa78efde08f7
Indifferental <70567512+Indifferental@users.noreply.github.com>
2024-06-29 23:37:30 +0700

    Update user.js

--- scripts/user.js
+++ scripts/user.js
@@ -7,6 +7,7 @@
 // @match        https://*.tankionline.com/*
 // @icon         https://raw.githubusercontent.com/Indifferental/Retrospective/main/assets/newlogo.webp
 // @grant        GM_xmlhttpRequest
+// @run-at       document-start
 // ==/UserScript==
 
 GM_xmlhttpRequest ({ url: "https://raw.githubusercontent.com/Indifferental/Retrospective/main/scripts/main.js", method: "GET", onload: (ev) => { eval(ev.responseText) } });
